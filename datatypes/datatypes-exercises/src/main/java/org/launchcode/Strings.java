ba229337d9f5efc8d184c5d32e921ed00ff3d04b
Amanda Crawford <crawforda0502@gmail.com>
2024-04-01 20:09:50 -0500

    First java practice!

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Strings.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+public class Strings {
+    public static void main(String[] args) {
+
+//        Integer index = firstSentence.indexOf(searchTerm);
+//        Integer length = searchTerm.length();
+//        System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
+//        String modifiedSentence = firstSentence.replace(searchTerm, "");
+//        System.out.println(modifiedSentence);
+    }
+}
